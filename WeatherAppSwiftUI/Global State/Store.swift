aa00637900bf26a55922464ca7456c58274b92ca
masa950724mi <s100.iizuka.masami@gmail.com>
2023-07-19 15:08:50 +0900

    温度表示の変更機能の追加

--- WeatherAppSwiftUI/Global State/Store.swift
+++ WeatherAppSwiftUI/Global State/Store.swift
@@ -2,7 +2,12 @@ import Foundation
 
 class Store: ObservableObject {
     
+    @Published var selectedUnit: TemperatureUnit = .kelvin
     @Published var weatherList: [WeatherViewModel] = [WeatherViewModel]()
+    
+    init() {
+        selectedUnit = UserDefaults.standard.unit
+    }
     
     func addWeather(_ weather: WeatherViewModel) {
         weatherList.append(weather)
